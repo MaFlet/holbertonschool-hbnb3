d41118f62494dc8cc9beb052a97043d412eeba1f
MaFlet <mfletcher2808@hotmail.com>
2024-11-05 11:31:47 +1100

    Adding init info for init and txt files

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -1,9 +1,20 @@
 from flask import Flask
 from flask_restx import Api
+from flask_sqlalchemy import SQLAlchemy
 from app.api.v1.users import api as users_ns
 from app.api.v1.amenities import api as amenities_ns
 from app.api.v1.places import api as places_ns
 from app.api.v1.reviews import api as reviews_ns
+
+db = SQLAlchemy()
+
+def create_app(config_class=config.DevelopmentConfig):
+    #
+    # Existent code with app Flask instance
+    #
+    db.init_app(app)
+
+    # ...
 
 def create_app():
     """ method used to create an app instance """
