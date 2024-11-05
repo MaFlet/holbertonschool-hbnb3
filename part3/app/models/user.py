8b735fc664bd2c5aabdca480f1062283b27631ad
MaFlet <mfletcher2808@hotmail.com>
2024-11-05 13:03:10 +1100

    Adding tables.sql

--- part3/app/models/user.py
+++ part3/app/models/user.py
@@ -1,23 +1,33 @@
 """ User model """
 
+from app.persistence import Base
 import uuid
 import re
 from datetime import datetime
+from flask_bcrypt import Bcrypt
+from sqlalchemy import Column, String, DateTime, Boolean
+from sqlalchemy.orm import relationship
 
-class User:
+bcrypt = Bcrypt()
+
+class User(Base):
     """ User class """
-    # NOTE: The attribute declarations below can be considered redundant
-    # but I still like to do it out of habit.
+    __tablename__ = 'users'
 
-    # id = None
-    # first_name = ""
-    # last_name = ""
-    # email = ""
-    # is_admin = False
-    # created_at = None
-    # updated_at = None
-    # places = []
-    # reviews = []
+    # Remember: if you have getters & setters for any of the attributes
+    # you can't use the same name for the attributes themselves
+
+    id = Column(String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
+    created_at = Column(DateTime, nullable=False, default=datetime.now())
+    updated_at = Column(DateTime, nullable=False, default=datetime.now())
+    _first_name = Column("first_name", String(50), nullable=False)
+    _last_name = Column("last_name", String(50), nullable=False)
+    _email = Column("email", String(120), nullable=False, unique=True)
+    _password = Column("password", String(128), nullable=False)
+    _is_admin = Column("is_admin", Boolean, default=False)
+    # reviews_r = relationship("Review", back_populates="user_r", cascade="delete, delete-orphan")
+    # properties_r = relationship("Place", back_populates="owner_r", cascade="delete, delete-orphan")
+
 
     def __init__(self, first_name, last_name, email, is_admin = False):
         # NOTE: Attributes that don't already exist will be
@@ -35,6 +45,8 @@ class User:
         self.is_admin = is_admin
         self.places = [] # List to store user-owned places
         self.reviews = [] # List to store user-written reviews
+         # The method will call the setter
+        self.hash_password(password)
 
     # --- Getters and Setters ---
     # Setters are actually called when values are assigned in the constructor!
@@ -117,6 +129,14 @@ class User:
     def add_review(self, review):
         """Add a review to the user."""
         self.reviews.append(review)
+
+    def hash_password(self, password):
+        """Hash the password before storing it."""
+        self.password = bcrypt.generate_password_hash(password).decode('utf-8')
+
+    def verify_password(self, password):
+        """Verify the hashed password."""
+        return bcrypt.check_password_hash(self.password, password)
 
     @staticmethod
     def email_exists(email):
